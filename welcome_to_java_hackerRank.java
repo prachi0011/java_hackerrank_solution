9a1670701d049d301cb6b860c58e980a6fdfa05e
prachi0011 <60473704+prachi0011@users.noreply.github.com>
2020-07-12 10:33:47 +0530

    Update welcome_to_java_hackerRank.java

--- welcome_to_java_hackerRank.java
+++ welcome_to_java_hackerRank.java
@@ -1,7 +1,9 @@
 /*
 Welcome to the world of Java! In this challenge, we practice printing to stdout.
 
-The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
+The code stubs in your editor declare a Solution class and a main method.
+ Complete the main method by copying the two lines of code below and pasting 
+them inside the body of your main method.
 
 System.out.println("Hello, World.");
 System.out.println("Hello, Java.");
@@ -24,7 +26,8 @@ Hello, Java.
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        /* Enter your code here. Print output to STDOUT.
+ Your class should be named Solution. */
     System.out.println("Hello, World.");
    System.out.println("Hello, Java.");
     }
